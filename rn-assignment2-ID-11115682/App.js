25b37c94389eac8b412a830fa80b520516b330bf
Bouffdaddy1z <derrickedus66@gmail.com>
2024-05-26 16:11:12 +0000

    added My name to text component

--- rn-assignment2-ID-11115682/App.js
+++ rn-assignment2-ID-11115682/App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>
+        My name is <Text style={styles.bold}>EDUSEI DERRICK AKUAMOAH</Text>
+      </Text>
     </View>
   );
 }
@@ -13,8 +14,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ADD8E6', // Change the background color
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24, // Increase the font size
+  },
+  bold: {
+    fontWeight: 'bold', // Make the name bold
   },
 });
